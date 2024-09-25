1de3d7459585e7868c1ce6f75f35b009fb3519b5
Faruks23 <mdfaruksp2@gmail.com>
2024-09-25 21:39:23 +0600

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './Component/Home/Home.jsx'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Component/Home/Home.jsx";
+import { SmoothScroll } from "./shared/SmothScroll.jsx";
 
 const routes = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App></App>,
     children: [
       {
-        path: '/',
-      element:<Home></Home>}
-    ]
-  }
-])
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={routes}></RouterProvider>
+    <SmoothScroll>
+      <RouterProvider router={routes}></RouterProvider>
+    </SmoothScroll>
   </React.StrictMode>
 );
---
+++ src/shared/SmothScroll.jsx
@@ -0,0 +1,9 @@
+import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
+
+export function SmoothScroll({children}) {
+  const lenis = useLenis(({ scroll }) => {
+    // called every scroll
+  });
+
+  return <ReactLenis root>{ children}</ReactLenis>;
+}
